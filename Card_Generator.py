1659cafc69ca65a842898e0ffa3143e7a5747a1e
RoshanJeyakumar <36739324+RoshanJeyakumar@users.noreply.github.com>
2021-12-20 21:49:58 +0530

    Add files via upload

---
+++ Card_Generator.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Automatic Card Generation from Template 
+# ## *Image Manipulation using Python*
+# 
+# ---
+# 
+# ```bash
+# pip install pillow
+# ```
+
+# In[1]:
+
+
+import pandas as pd
+from PIL import Image, ImageDraw, ImageFont
+
+
+# In[2]:
+
+
+df = pd.read_csv("PAN_data.csv")
+
+
+# In[3]:
+
+
+df
+
+
+# In[4]:
+
+
+records = df.to_dict(orient='records')
+
+
+
+# In[5]:
+
+
+font = ImageFont.truetype("OpenSans-Semibold.ttf", size=23)
+
+
+# In[6]:
+
+
+def generate_card(data):
+    template = Image.open("PAN_card.jpg")
+    draw = ImageDraw.Draw(template)
+    draw.text((31,273), str(data['Name']), font=font, fill='black')
+    draw.text((30,406), data['Date of Birth'], font=font, fill='black')
+    return template
+
+
+# In[7]:
+
+i = 0
+for record in records:
+    i+=1
+    card = generate_card(record)
+    card.save("Record" + str(i) + ".jpg")
+
---
+++ Pan_Data_Faker.py
@@ -0,0 +1,20 @@
+import csv
+from faker import Faker
+import datetime
+
+def datagenerate(records, headers):
+    fake = Faker('en_IN')
+    with open("PAN_data.csv", 'wt') as csvFile:
+        writer = csv.DictWriter(csvFile, fieldnames=headers)
+        writer.writeheader()
+        for i in range(records):            
+            writer.writerow({
+                    "Name": fake.name(),
+                    "Date of Birth" : fake.date(pattern="%d-%m-%Y", end_datetime=datetime.date(2000, 1,1)),
+                    })
+    
+if __name__ == '__main__':
+    records = 10
+    headers = [ "Name", "Date of Birth"]
+    datagenerate(records, headers)
+    print("CSV generation complete!")
